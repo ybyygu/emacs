7ce2b324959dd69ce4153cf532edc0ae1a98124e
Wenping Guo <winpng@gmail.com>
2017-09-20 13:59:53 +0800

    弃用zotxt, 改用自定义设置来支持zotero协议.

--- init.el
+++ init.el
@@ -1,15 +1,4 @@
-;; install
-;; 官方首页提供了三种安装方式. 迁移的时候考虑使用第二种或第三种.
-
-;; 1. spacemacs 目录放置到~/etc/spacemacs/.emacs.d
-;;    : git clone https://github.com/syl20bnr/spacemacs.git ~/etc/spacemacs/.emacs.d
-
-;; 2. 修改配置文件中的路径
-;;    elpa 默认仍是存在~/.emacs.d/elpa 下, 可以用 [[https://www.gnu.org/software/emacs/manual/html_node/emacs/Package-Files.html][package-user-dir]] 修改.
-
-;;    #+header: :tangle init.el
-
-(setq spacemacs-start-directory "~/Install/configs/spacemacs/.emacs.d/")
+(setq spacemacs-start-directory "~/etc/spacemacs/.emacs.d/")
 (setq package-user-dir (file-name-as-directory
                         (concat spacemacs-start-directory "elpa/")))
 (load-file (concat spacemacs-start-directory "init.el"))
--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #! /bin/sh
-
-
-# 3. 命令行启动 emacs
-#    #+header: :tangle start.sh
-#    #+header: :shebang #! /bin/sh
-
 # HOME=`pwd` emacs
 # emacs -q -l ...
-emacs --no-init-file --load ~/Install/configs/spacemacs/init.el
+emacs --no-init-file --load ~/etc/spacemacs/init.el

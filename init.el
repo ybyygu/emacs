39d985b8b9c6930246f2474956322f999ebdf492
Wenping Guo <winpng@gmail.com>
2017-09-21 10:03:13 +0800

    同步到~/Install目录下

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-(setq spacemacs-start-directory "~/etc/spacemacs/.emacs.d/")
+(setq spacemacs-start-directory "~/Install/config/spacemacs/.emacs.d/")
 (setq package-user-dir (file-name-as-directory
                         (concat spacemacs-start-directory "elpa/")))
 (load-file (concat spacemacs-start-directory "init.el"))

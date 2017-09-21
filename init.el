739082a6ba23ebfc0102f489cf34a9781b8dd187
Wenping Guo <winpng@gmail.com>
2017-09-21 10:34:34 +0800

    将org-mode生成的文件移出git

--- init.el
+++
@@ -1,4 +0,0 @@
-(setq spacemacs-start-directory "~/Install/config/spacemacs/.emacs.d/")
-(setq package-user-dir (file-name-as-directory
-                        (concat spacemacs-start-directory "elpa/")))
-(load-file (concat spacemacs-start-directory "init.el"))
--- start.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-# HOME=`pwd` emacs
-# emacs -q -l ...
-emacs --no-init-file --load ~/etc/spacemacs/init.el

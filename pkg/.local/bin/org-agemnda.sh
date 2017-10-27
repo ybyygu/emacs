9dcd19f4f3f66e9ac2acc6b3263371a723cdf7d4
Wenping Guo <ybyygu@gmail.com>
2017-10-27 14:55:10 +0800

    使用stow来安装tangle出的文件

---
+++ pkg/.local/bin/org-agemnda.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+# [[file:~/Install/configs/spacemacs/config.note::97e265a3-58ad-40b4-9d45-432f8cd691e1][97e265a3-58ad-40b4-9d45-432f8cd691e1]]
+emacsclient -c -F "((fullscreen . maximized))" -e "(org-agenda nil \"a\")" -e "(delete-other-windows)"
+# 97e265a3-58ad-40b4-9d45-432f8cd691e1 ends here

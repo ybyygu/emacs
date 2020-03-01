40898f212846c72a71f34c78b2896b51c5d23d7f
Wenping Guo <ybyygu@gmail.com>
2020-03-01 15:38:30 +0800

    优化workspace相关按键

--- pkg/.doom.d/bindings.el
+++ pkg/.doom.d/bindings.el
@@ -144,34 +144,7 @@
 (map! (:when (featurep! :ui popup)
         :n "C-`"   #'+popup/toggle
         :n "C-~"   #'+popup/raise
-        :g "C-x p" #'+popup/other)
-
-      (:when (featurep! :ui workspaces)
-        :n "C-t"   #'+workspace/new
-        :n "C-S-t" #'+workspace/display
-        :g "M-1"   #'+workspace/switch-to-0
-        :g "M-2"   #'+workspace/switch-to-1
-        :g "M-3"   #'+workspace/switch-to-2
-        :g "M-4"   #'+workspace/switch-to-3
-        :g "M-5"   #'+workspace/switch-to-4
-        :g "M-6"   #'+workspace/switch-to-5
-        :g "M-7"   #'+workspace/switch-to-6
-        :g "M-8"   #'+workspace/switch-to-7
-        :g "M-9"   #'+workspace/switch-to-8
-        :g "M-0"   #'+workspace/switch-to-final
-        (:when IS-MAC
-          :g "s-t"   #'+workspace/new
-          :g "s-T"   #'+workspace/display
-          :n "s-1"   #'+workspace/switch-to-0
-          :n "s-2"   #'+workspace/switch-to-1
-          :n "s-3"   #'+workspace/switch-to-2
-          :n "s-4"   #'+workspace/switch-to-3
-          :n "s-5"   #'+workspace/switch-to-4
-          :n "s-6"   #'+workspace/switch-to-5
-          :n "s-7"   #'+workspace/switch-to-6
-          :n "s-8"   #'+workspace/switch-to-7
-          :n "s-9"   #'+workspace/switch-to-8
-          :n "s-0"   #'+workspace/switch-to-final)))
+        :g "C-x p" #'+popup/other))
 
 ;;; :editor
 (map! (:when (featurep! :editor format)
@@ -241,28 +214,23 @@
       ;;; <leader> l --- workspace
       (:when (featurep! :ui workspaces)
         (:prefix-map ("l" . "workspace")
-          :desc "Display tab bar"           "TAB" #'+workspace/display
-          :desc "Switch workspace"          "."   #'+workspace/switch-to
-          :desc "Switch to last workspace"  "`"   #'+workspace/other
-          :desc "New workspace"             "n"   #'+workspace/new
           :desc "Load workspace from file"  "l"   #'+workspace/load
           :desc "Save workspace to file"    "s"   #'+workspace/save
-          :desc "Delete session"            "x"   #'+workspace/kill-session
           :desc "Delete this workspace"     "d"   #'+workspace/delete
+          :desc "Next workspace"            "n"   #'+workspace/switch-right
+          :desc "Previous workspace"        "p"   #'+workspace/switch-left
+          :desc "Switch to last workspace"  "b"   #'+workspace/other
+          :desc "Display tab bar"           "."   #'+workspace/display
+          :desc "Switch workspace"          "TAB" #'+workspace/switch-to
+          :desc "New workspace"             "N"   #'+workspace/new
+          :desc "Delete session"            "x"   #'+workspace/kill-session
           :desc "Rename workspace"          "r"   #'+workspace/rename
           :desc "Restore last session"      "R"   #'+workspace/restore-last-session
-          :desc "Next workspace"            "]"   #'+workspace/switch-right
-          :desc "Previous workspace"        "["   #'+workspace/switch-left
           :desc "Switch to 1st workspace"   "1"   #'+workspace/switch-to-0
           :desc "Switch to 2nd workspace"   "2"   #'+workspace/switch-to-1
           :desc "Switch to 3rd workspace"   "3"   #'+workspace/switch-to-2
           :desc "Switch to 4th workspace"   "4"   #'+workspace/switch-to-3
-          :desc "Switch to 5th workspace"   "5"   #'+workspace/switch-to-4
-          :desc "Switch to 6th workspace"   "6"   #'+workspace/switch-to-5
-          :desc "Switch to 7th workspace"   "7"   #'+workspace/switch-to-6
-          :desc "Switch to 8th workspace"   "8"   #'+workspace/switch-to-7
-          :desc "Switch to 9th workspace"   "9"   #'+workspace/switch-to-8
-          :desc "Switch to final workspace" "0"   #'+workspace/switch-to-final))
+          ))
 
       ;;; <leader> b --- buffer
       (:prefix-map ("b" . "buffer")

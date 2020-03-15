0c3a1193518109ddfa0e8fccc7746aad035ac7e8
Wenping Guo <ybyygu@gmail.com>
2020-03-15 08:37:45 +0800

    company开启tng feature

--- pkg/.doom.d/init.el
+++ pkg/.doom.d/init.el
@@ -17,10 +17,10 @@
 (doom! :input
 
        :completion
-       company           ; the ultimate code completion backend
+       (company +tng)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +fuzzy +prescient)               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs

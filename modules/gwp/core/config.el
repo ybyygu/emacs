0cdb108a4220e5631630eeaee02734b2383ee484
Wenping Guo <ybyygu@gmail.com>
2022-03-03 19:17:02 +0800

    meow: 更新至最新版, 解决 beacon 模式下 M-d 的问题

--- modules/gwp/core/config.el
+++ modules/gwp/core/config.el
@@ -130,8 +130,8 @@
    '("C-v" . meow-grab)
    ;; 特殊功能
    '("]" . sp-unwrap-sexp)
-   '("Q" . meow-start-kmacro)                   ; 宏编辑: record
-   '("R" . meow-beacon-end-and-apply-kmacro)    ; 宏应用: repeat
+   ;; '("Q" . meow-start-kmacro)                   ; 宏编辑: record
+   ;; '("R" . meow-beacon-end-and-apply-kmacro)    ; 宏应用: repeat
    '("$" . ispell-word)
    '("'" . repeat)                      ; 重复上一个命令
    '("=" . meow-goto-line)
--- modules/gwp/core/packages.el
+++ modules/gwp/core/packages.el
@@ -1,3 +1,3 @@
 ;; [[file:../../../gwp.note::db68897a][db68897a]]
-(package! meow :pin "b494651")
+(package! meow :pin "aa4aa68")
 ;; db68897a ends here
